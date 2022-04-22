90ded151b45de2f7d1d0d4a91f3f5101a646366f
David Villegas <david.villegasg7@gmail.com>
2022-04-21 22:48:17 -0400

    FIX: Modificando App para su funcionamiento

--- node/connectionDb.js
+++ node/connectionDb.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 const config = {
-    host: process.env.HOST || 'db',
+    host: process.env.HOST || '127.0.0.1',
     user: process.env.USER || 'root',
     password: process.env.PASSWORD || 'root',
-    database: process.env.DATABASE || 'nodedb'
+    database: process.env.DATABASE || 'node_db'
 };
 
 const mysql = require('mysql');
